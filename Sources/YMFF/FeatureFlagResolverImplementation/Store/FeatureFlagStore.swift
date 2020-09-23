696bf436ceaee94a8e4fd16d582957e1e08db4a5
Yakov Manshin <git@yakovmanshin.com>
2020-09-24 02:16:55 +0300

    FeatureFlagStore

---
+++ Sources/YMFF/FeatureFlagResolverImplementation/Store/FeatureFlagStore.swift
@@ -0,0 +1,29 @@
+//
+//  FeatureFlagStore.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 9/20/20.
+//  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
+//
+
+// MARK: - FeatureFlagStore
+
+public enum FeatureFlagStore {
+    case opaque(FeatureFlagStoreProtocol)
+    case transparent(TransparentFeatureFlagStore)
+}
+
+// MARK: - FeatureFlagStoreProtocol
+
+extension FeatureFlagStore: FeatureFlagStoreProtocol {
+    
+    public func value(forKey key: String) -> Any? {
+        switch self {
+        case .opaque(let store):
+            return store.value(forKey: key)
+        case .transparent(let store):
+            return store[key]
+        }
+    }
+    
+}
---
+++ Sources/YMFF/FeatureFlagResolverImplementation/Store/TransparentFeatureFlagStore.swift
@@ -0,0 +1,9 @@
+//
+//  TransparentFeatureFlagStore.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 9/20/20.
+//  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
+//
+
+public typealias TransparentFeatureFlagStore = [String : Any]
