3ef4264499ac9ab5c93771c1257a4f2df330ef28
Yakov Manshin <git@yakovmanshin.com>
2020-09-24 23:36:47 +0300

    FeatureFlagResolver Core

---
+++ Sources/YMFF/FeatureFlagResolverImplementation/FeatureFlagResolver.swift
@@ -0,0 +1,23 @@
+//
+//  FeatureFlagResolver.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 9/20/20.
+//  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
+//
+
+// MARK: - FeatureFlagResolver
+
+final public class FeatureFlagResolver {
+    
+    // MARK: Properties
+    
+    let configuration: FeatureFlagResolverConfigurationProtocol
+    
+    // MARK: Initializers
+    
+    public init(configuration: FeatureFlagResolverConfiguration) {
+        self.configuration = configuration
+    }
+    
+}
