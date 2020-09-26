8b20e24f845ac410e825a4e276e79008ec51e66d
Yakov Manshin <git@yakovmanshin.com>
2020-09-26 14:46:11 +0300

    RuntimeOverridesStore

---
+++ Sources/YMFF/FeatureFlagResolverImplementation/Store/RuntimeOverridesStore.swift
@@ -0,0 +1,37 @@
+//
+//  RuntimeOverridesStore.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 9/26/20.
+//  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
+//
+
+// MARK: - RuntimeOverridesStore
+
+final public class RuntimeOverridesStore {
+    
+    private var store: TransparentFeatureFlagStore
+    
+    public init() {
+        store = .init()
+    }
+    
+}
+
+// MARK: - MutableFeatureFlagStoreProtocol
+
+extension RuntimeOverridesStore: MutableFeatureFlagStoreProtocol {
+    
+    public func value(forKey key: String) -> Any? {
+        store[key]
+    }
+    
+    public func setValue(_ value: Any, forKey key: String) {
+        store[key] = value
+    }
+    
+    public func removeValue(forKey key: String) {
+        store.removeValue(forKey: key)
+    }
+    
+}
