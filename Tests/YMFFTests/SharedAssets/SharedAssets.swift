5b2d0e30572225e6fafc6f642c9ae4034ebba9c4
Yakov Manshin <git@yakovmanshin.com>
2020-09-26 13:44:39 +0300

    Feature Flag (#8)

---
+++ Tests/YMFFTests/SharedAssets/SharedAssets.swift
@@ -0,0 +1,53 @@
+//
+//  SharedAssets.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 9/26/20.
+//  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
+//
+
+import YMFF
+
+// MARK: - Shared
+
+enum SharedAssets {
+    
+    static var configuration = FeatureFlagResolverConfiguration(
+        localStore: .transparent(localStore),
+        remoteStore: .opaque(OpaqueStoreStab(store: remoteStore))
+    )
+    
+    private static var localStore: [String : Any] { [
+        "bool": false,
+        "int": 123,
+        "string": "STRING_VALUE_LOCAL",
+        "optionalIntNonNil": Optional<Int>.some(123) as Any
+    ] }
+    
+    private static var remoteStore: [String : Any] { [
+        "bool": true,
+        "string": "STRING_VALUE_REMOTE",
+        "optionalIntNil": Optional<Int>.none as Any,
+        "optionalIntNonNil": Optional<Int>.some(456) as Any
+    ] }
+    
+    static var boolKey: FeatureFlagKey { .init("bool") }
+    static var intKey: FeatureFlagKey { .init("int") }
+    static var stringKey: FeatureFlagKey { .init("string") }
+    static var optionalIntNilKey: FeatureFlagKey { .init("optionalIntNil") }
+    static var optionalIntNonNilKey: FeatureFlagKey { .init("optionalIntNonNil") }
+    static var nonexistentKey: FeatureFlagKey { .init("nonexistent") }
+    
+}
+
+// MARK: - Supplementary Types
+
+fileprivate struct OpaqueStoreStab: FeatureFlagStoreProtocol {
+    
+    let store: TransparentFeatureFlagStore
+    
+    func value(forKey key: String) -> Any? {
+        store[key]
+    }
+    
+}
