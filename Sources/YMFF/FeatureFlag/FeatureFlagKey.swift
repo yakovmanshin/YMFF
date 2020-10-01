8e471682d39ba5d9c8c93e19e9f4dce703cc2a60
Yakov Manshin <git@yakovmanshin.com>
2020-10-01 21:12:02 +0300

    Documented FeatureFlagKey

--- Sources/YMFF/FeatureFlag/FeatureFlagKey.swift
+++ Sources/YMFF/FeatureFlag/FeatureFlagKey.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
 //
 
+/// A structure that contains information about the keys used to retrieve feature flag values from the corresponding stores.
 public struct FeatureFlagKey {
     
     let localKey: String
@@ -16,6 +17,9 @@ public struct FeatureFlagKey {
         self.remoteKey = remoteKey
     }
     
+    /// Initializes a `FeatureFlagKey` instance with the same key used for local and remote stores.
+    ///
+    /// - Parameter key: *Required.* The key to use for value retrieval.
     public init(_ key: String) {
         self.init(local: key, remote: key)
     }
