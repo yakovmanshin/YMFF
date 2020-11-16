964ceced392a3473d0a2a41bff08bbb5a7e7f41c
Yakov Manshin <git@yakovmanshin.com>
2020-11-16 20:22:03 +0300

    Updated Tests

--- Tests/YMFFTests/FeatureFlagResolverTests.swift
+++ Tests/YMFFTests/FeatureFlagResolverTests.swift
@@ -32,12 +32,7 @@ extension FeatureFlagResolverTests {
     func testIntValueResolution() {
         let expectedValue = 123
         
-        XCTAssertEqual(resolver.value(for: SharedAssets.intKey), expectedValue)
-        XCTAssertEqual(try resolveIntValue(for: SharedAssets.intKey), expectedValue)
-    }
-    
-    private func resolveIntValue(for key: FeatureFlagKey) throws -> Int {
-        try resolver._value(for: key)
+        XCTAssertEqual(try resolver.value(for: SharedAssets.intKey), expectedValue)
     }
     
     // MARK: String Values
@@ -45,63 +40,25 @@ extension FeatureFlagResolverTests {
     func testStringValueResolution() {
         let expectedValue = "STRING_VALUE_REMOTE"
         
-        XCTAssertEqual(resolver.value(for: SharedAssets.stringKey), expectedValue)
-        XCTAssertEqual(try resolveStringValue(for: SharedAssets.stringKey), expectedValue)
-    }
-    
-    private func resolveStringValue(for key: FeatureFlagKey) throws -> String {
-        try resolver._value(for: key)
+        XCTAssertEqual(try resolver.value(for: SharedAssets.stringKey), expectedValue)
     }
     
     // MARK: Optional Values
     
-    func testOptionalIntNilValueResolution() {
-        let expectedValue: Int? = nil
-        
-        XCTAssertEqual(resolver.value(for: SharedAssets.optionalIntNilKey), expectedValue)
+    func testOptionalIntValueResolution() {
         do {
-            _ = try resolveOptionalValue(for: SharedAssets.optionalIntNilKey)
+            let _: Int? = try resolver.value(for: SharedAssets.optionalIntKey)
             XCTFail()
         } catch FeatureFlagResolverError.optionalValuesNotAllowed { } catch { XCTFail() }
-    }
-    
-    func testOptionalIntNonNilValueResolution() {
-        let expectedValue: Int? = nil
-        
-        XCTAssertEqual(resolver.value(for: SharedAssets.optionalIntNonNilKey), expectedValue)
-        do {
-            _ = try resolveOptionalValue(for: SharedAssets.optionalIntNonNilKey)
-            XCTFail()
-        } catch FeatureFlagResolverError.optionalValuesNotAllowed { } catch { XCTFail() }
-    }
-    
-    private func resolveOptionalValue(for key: FeatureFlagKey) throws -> Int? {
-        try resolver._value(for: key)
     }
     
     // MARK: Nonexistent Values
 
     func testNonexistentValueResolution() throws {
-        let expectedValue: Int? = nil
-        
-        XCTAssertEqual(resolver.value(for: SharedAssets.nonexistentKey), expectedValue)
         do {
-            _ = try resolveNonexistentValue(for: SharedAssets.nonexistentKey)
+            let _: Int = try resolver.value(for: SharedAssets.nonexistentKey)
             XCTFail()
-        } catch FeatureFlagResolverError.noStoreContainsValueForKey { } catch { XCTFail() }
-    }
-    
-    private func resolveNonexistentValue(for key: FeatureFlagKey) throws -> Int {
-        try resolver._value(for: key)
-    }
-    
-    // MARK: Value Type Casting
-    
-    func testValueTypeCasting() {
-        do {
-            _ = try resolveStringValue(for: SharedAssets.intKey)
-            XCTFail()
-        } catch FeatureFlagResolverError.typeMismatch { } catch { XCTFail() }
+        } catch FeatureFlagResolverError.valueNotFoundInPersistentStores { } catch { XCTFail() }
     }
     
     // MARK: Runtime Override
@@ -111,40 +68,43 @@ extension FeatureFlagResolverTests {
         let originalValue = 123
         let overrideValue = 789
         
-        XCTAssertEqual(resolver.value(for: key), originalValue)
+        XCTAssertEqual(try resolver.value(for: key), originalValue)
         
         XCTAssertNoThrow(try resolver.overrideInRuntime(key, with: overrideValue))
         
-        XCTAssertEqual(resolver.value(for: key), overrideValue)
+        XCTAssertEqual(try resolver.value(for: key), overrideValue)
         
         resolver.removeRuntimeOverride(for: key)
         
-        XCTAssertEqual(resolver.value(for: key), originalValue)
+        XCTAssertEqual(try resolver.value(for: key), originalValue)
     }
     
     func testRuntimeOverrideFailure() {
         let key = SharedAssets.stringKey
         let overrideValue = 789
         
-        XCTAssertEqual(resolver.value(for: key), "STRING_VALUE_REMOTE")
+        XCTAssertEqual(try resolver.value(for: key), "STRING_VALUE_REMOTE")
         
         do {
             _ = try resolver.overrideInRuntime(key, with: overrideValue)
             XCTFail()
         } catch FeatureFlagResolverError.typeMismatch { } catch { XCTFail() }
         
-        XCTAssertEqual(resolver.value(for: key), "STRING_VALUE_REMOTE")
+        XCTAssertEqual(try resolver.value(for: key), "STRING_VALUE_REMOTE")
     }
     
     func testRuntimeOverrideForNewKeys() {
         let key = FeatureFlagKey("NEW_KEY")
         let overrideValue = 789
         
-        XCTAssertNil(resolver.value(for: key))
+        do {
+            let _: Int = try resolver.value(for: key)
+            XCTFail()
+        } catch FeatureFlagResolverError.valueNotFoundInPersistentStores { } catch { XCTFail() }
         
         XCTAssertNoThrow(try resolver.overrideInRuntime(key, with: overrideValue))
         
-        XCTAssertEqual(resolver.value(for: key), overrideValue)
+        XCTAssertEqual(try resolver.value(for: key), overrideValue)
     }
     
 }
@@ -158,7 +118,7 @@ extension FeatureFlagResolverTests {
     func testValueRetrieval() {
         let key = "int"
         
-        XCTAssertNoThrow(try resolver.retrieveValueFromFirstStore(of: resolver.configuration.persistentStores, containingKey: key))
+        XCTAssertNoThrow(try resolver.retrieveFirstValueFoundInPersistentStores(byKey: key) as Int)
     }
     
     func testValueRetrievalFromEmptyPersistentStoresArray() {
@@ -167,8 +127,8 @@ extension FeatureFlagResolverTests {
         let key = "int"
         
         do {
-            _ = try resolver.retrieveValueFromFirstStore(of: resolver.configuration.persistentStores, containingKey: key)
-        } catch FeatureFlagResolverError.persistentStoresIsEmpty { } catch { XCTFail() }
+            let _: Int = try resolver.retrieveFirstValueFoundInPersistentStores(byKey: key)
+        } catch FeatureFlagResolverError.noPersistentStoreAvailable { } catch { XCTFail() }
     }
     
     // MARK: Value Validation
@@ -193,27 +153,6 @@ extension FeatureFlagResolverTests {
         
         XCTAssertFalse(resolver.valueIsOptional(nonOptionalValue))
         XCTAssertTrue(resolver.valueIsOptional(optionalValue as Any))
-    }
-    
-    // MARK: Value Type Casting
-    
-    func testValueTypeCastingSuccess() {
-        let int = 123
-        let intAsAny = int as Any
-        let targetType = Int.self
-        
-        XCTAssertEqual(try resolver.cast(intAsAny, to: targetType), int)
-    }
-    
-    func testValueTypeCastingFailure() {
-        let int = 123
-        let intAsAny = int as Any
-        let targetType = String.self
-        
-        do {
-            _ = try resolver.cast(intAsAny, to: targetType)
-            XCTFail()
-        } catch FeatureFlagResolverError.typeMismatch { } catch { XCTFail() }
     }
     
     // MARK: Override Value Validation
