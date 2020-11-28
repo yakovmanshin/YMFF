eca9f0a685c15fefad1f9445e46f681b54c2cab6
Yakov Manshin <git@yakovmanshin.com>
2020-11-28 15:24:07 +0300

    Documentation Improvements

--- Sources/YMFF/FeatureFlagResolver/Store/MutableFeatureFlagStoreProtocol.swift
+++ Sources/YMFF/FeatureFlagResolver/Store/MutableFeatureFlagStoreProtocol.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Yakov Manshin. See the LICENSE file for license info.
 //
 
-/// An object that stores feature flag values set in the runtime.
+/// An object that stores feature flag values that can be added and removed in runtime.
 public protocol MutableFeatureFlagStoreProtocol: AnyObject, FeatureFlagStoreProtocol {
     
     /// Adds the value to the store so it can be retrieved with the key later.
