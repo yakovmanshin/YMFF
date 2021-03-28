e54c5106864de603ee9f163761d14f4cd74e12fd
Yakov Manshin <git@yakovmanshin.com>
2021-03-28 14:09:03 +0300

    [#38] UserDefaults Store (#42)
    * Introduced `UserDefaultsStore` which can be used as both read-only and mutable store
    * Extended `FeatureFlagStore` with the new `userDefaults(_:)` case

---
+++ Sources/YMFF/FeatureFlagResolverImplementation/Store/UserDefaultsStore.swift
@@ -0,0 +1,48 @@
+//
+//  UserDefaultsStore.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 3/25/21.
+//  Copyright © 2021 Yakov Manshin. See the LICENSE file for license info.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+// MARK: - UserDefaultsStore
+
+/// An object that provides read and write access to feature flag values store in `UserDefaults`.
+final public class UserDefaultsStore {
+    
+    private let userDefaults: UserDefaults
+    
+    /// Initializes a new `UserDefaultsStore`.
+    ///
+    /// - Parameter userDefaults: *Optional.* The `UserDefaults` object used to read and write values.
+    /// `UserDefaults.standard` is used by default.
+    public init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+    }
+    
+}
+
+// MARK: - MutableFeatureFlagStoreProtocol
+
+extension UserDefaultsStore: MutableFeatureFlagStoreProtocol {
+    
+    public func value<Value>(forKey key: String) -> Value? {
+        userDefaults.value(forKey: key) as? Value
+    }
+    
+    public func setValue<Value>(_ value: Value, forKey key: String) {
+        userDefaults.setValue(value, forKey: key)
+    }
+    
+    public func removeValue(forKey key: String) {
+        userDefaults.removeObject(forKey: key)
+    }
+    
+}
+
+#endif
