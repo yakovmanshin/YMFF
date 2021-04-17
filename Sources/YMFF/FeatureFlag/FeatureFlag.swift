782fb591501e36a93e33c3b2753558c666e058c3
Yakov Manshin <git@yakovmanshin.com>
2021-04-17 22:42:19 +0300

    removeValueFromMutableStore()

--- Sources/YMFF/FeatureFlag/FeatureFlag.swift
+++ Sources/YMFF/FeatureFlag/FeatureFlag.swift
@@ -56,10 +56,12 @@ final public class FeatureFlag<Value> {
     /// The object returned when referencing the feature flag with a dollar sign (`$`).
     public var projectedValue: FeatureFlag<Value> { self }
     
-    // MARK: Runtime Overrides
+    // MARK: Mutable Value Removal
     
-    /// Removes the feature flag value that overrides persistent values in runtime.
-    public func removeRuntimeOverride() {
+    /// Removes the value from the first mutable feature flag store which contains one for `key`.
+    ///
+    /// + Errors thrown by `resolver` are ignored.
+    public func removeValueFromMutableStore() {
         try? resolver.removeValueFromMutableStore(using: key)
     }
     
