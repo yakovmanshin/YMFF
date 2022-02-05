66fc1c48df27bbc920250821c1ce8a095b20d337
Yakov Manshin <git@yakovmanshin.com>
2022-02-05 18:38:36 +0300

    [#82] Added Support for Raw Values (#83)
    * Extended `FeatureFlag` with support for raw values
    * Introduced `FeatureFlagValueTransformer` for converting between native and raw values
    * Updated tests

--- Sources/YMFF/FeatureFlag/FeatureFlag.swift
+++ Sources/YMFF/FeatureFlag/FeatureFlag.swift
@@ -12,12 +12,14 @@ import YMFFProtocols
 
 /// An object that facilitates access to feature flag values.
 @propertyWrapper
-final public class FeatureFlag<Value> {
+final public class FeatureFlag<RawValue, Value> {
     
     // MARK: Properties
     
     /// The key used to retrieve feature flag values.
     public let key: FeatureFlagKey
+
+    private let transformer: FeatureFlagValueTransformer<RawValue, Value>
     
     /// The fallback value returned when no store is able to provide the real one.
     public let defaultValue: Value
@@ -30,16 +32,33 @@ final public class FeatureFlag<Value> {
     ///
     /// - Parameters:
     ///   - key: *Required.* The key used to address feature flag values in stores.
+    ///   - transformer: *Required.* The object that transforms raw values into values, and vice versa.
     ///   - defaultValue: *Required.* The value returned in case all stores fail to provide a value.
     ///   - resolver: *Required.* The resolver object used to retrieve values from stores.
     public init(
         _ key: FeatureFlagKey,
+        transformer: FeatureFlagValueTransformer<RawValue, Value>,
         default defaultValue: Value,
         resolver: FeatureFlagResolverProtocol
     ) {
         self.key = key
+        self.transformer = transformer
         self.defaultValue = defaultValue
         self.resolver = resolver
+    }
+    
+    /// Creates a new `FeatureFlag` with value and raw value of the same type.
+    ///
+    /// - Parameters:
+    ///   - key: *Required.* The key used to address feature flag values in stores.
+    ///   - defaultValue: *Required.* The value returned in case all stores fail to provide a value.
+    ///   - resolver: *Required.* The resolver object used to retrieve values from stores.
+    public convenience init(
+        _ key: FeatureFlagKey,
+        default defaultValue: Value,
+        resolver: FeatureFlagResolverProtocol
+    ) where RawValue == Value {
+        self.init(key, transformer: .identity, default: defaultValue, resolver: resolver)
     }
     
     // MARK: Wrapped Value
@@ -47,16 +66,21 @@ final public class FeatureFlag<Value> {
     /// The resolved value of the feature flag.
     public var wrappedValue: Value {
         get {
-            (try? (resolver.value(for: key) as Value)) ?? defaultValue
+            guard
+                let rawValue = try? (resolver.value(for: key) as RawValue),
+                let value = transformer.valueFromRawValue(rawValue)
+            else { return defaultValue }
+            
+            return value
         } set {
-            try? resolver.setValue(newValue, toMutableStoreUsing: key)
+            try? resolver.setValue(transformer.rawValueFromValue(newValue), toMutableStoreUsing: key)
         }
     }
     
     // MARK: Projected Value
     
     /// The object returned when referencing the feature flag with a dollar sign (`$`).
-    public var projectedValue: FeatureFlag<Value> { self }
+    public var projectedValue: FeatureFlag<RawValue, Value> { self }
     
     // MARK: Mutable Value Removal
     
---
+++ Sources/YMFF/FeatureFlag/FeatureFlagValueTransformer.swift
@@ -0,0 +1,36 @@
+//
+//  FeatureFlagValueTransformer.swift
+//  YMFF
+//
+//  Created by Yakov Manshin on 2/5/22.
+//  Copyright © 2022 Yakov Manshin. See the LICENSE file for license info.
+//
+
+// MARK: - Transformer
+
+/// An object used by `FeatureFlag` to transform raw values into native values, and vice versa.
+public struct FeatureFlagValueTransformer<RawValue, Value> {
+    
+    let valueFromRawValue: (RawValue) -> Value?
+    let rawValueFromValue: (Value) -> RawValue
+    
+    /// Creates a new instance of transformer using the specified transformation closures.
+    public init(
+        valueFromRawValue: @escaping (RawValue) -> Value?,
+        rawValueFromValue: @escaping (Value) -> RawValue
+    ) {
+        self.valueFromRawValue = valueFromRawValue
+        self.rawValueFromValue = rawValueFromValue
+    }
+    
+}
+
+// MARK: - Identity
+
+extension FeatureFlagValueTransformer where RawValue == Value {
+    
+    static var identity: FeatureFlagValueTransformer {
+        .init(valueFromRawValue: { $0 }, rawValueFromValue: { $0 })
+    }
+    
+}
