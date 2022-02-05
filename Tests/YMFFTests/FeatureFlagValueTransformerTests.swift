dcf3e21d03f176bc906f27eb76f447564452f3e0
Yakov Manshin <git@yakovmanshin.com>
2022-02-05 17:12:30 +0300

    `FeatureFlagValueTransformerTests`

---
+++ Tests/YMFFTests/FeatureFlagValueTransformerTests.swift
@@ -0,0 +1,128 @@
+//
+//  FeatureFlagValueTransformerTests.swift
+//  YMFFTests
+//
+//  Created by Yakov Manshin on 2/5/22.
+//  Copyright © 2022 Yakov Manshin. See the LICENSE file for license info.
+//
+
+import XCTest
+
+@testable import YMFF
+
+// MARK: - Tests
+
+final class FeatureFlagValueTransformerTests: XCTestCase {
+    
+    func testIdentityTransformer() {
+        let transformer: FeatureFlagValueTransformer<Int, Int> = .identity
+        
+        let intValue = 123
+        
+        XCTAssertEqual(transformer.valueFromRawValue(intValue), intValue)
+        XCTAssertEqual(transformer.rawValueFromValue(intValue), intValue)
+    }
+    
+    func testSameTypeTransformation() {
+        let transformer = FeatureFlagValueTransformer { string in
+            String(string.dropFirst(4))
+        } rawValueFromValue: { string in
+            "RAW_\(string)"
+        }
+        
+        let stringValue = "some_value"
+        let stringRawValue = "RAW_some_value"
+        
+        XCTAssertEqual(transformer.valueFromRawValue(stringRawValue), stringValue)
+        XCTAssertEqual(transformer.rawValueFromValue(stringValue), stringRawValue)
+    }
+    
+    func testStringToBoolTransformation() {
+        let transformer = FeatureFlagValueTransformer { string in
+            string == "true"
+        } rawValueFromValue: { bool in
+            bool ? "true" : "false"
+        }
+        
+        let stringRawValueTrue = "true"
+        let stringRawValueFalse = "false"
+        let stringRawValueOther = "OTHER"
+        
+        XCTAssertTrue(transformer.valueFromRawValue(stringRawValueTrue))
+        XCTAssertFalse(transformer.valueFromRawValue(stringRawValueFalse))
+        XCTAssertFalse(transformer.valueFromRawValue(stringRawValueOther))
+    }
+    
+    func testStringToEnumWithRawValueTransformation() {
+        let transformer = FeatureFlagValueTransformer { string in
+            AdType(rawValue: string) ?? .none
+        } rawValueFromValue: { type in
+            type.rawValue
+        }
+        
+        let stringRawValueNone = "none"
+        let stringRawValueBanner = "banner"
+        let stringRawValueVideo = "video"
+        let stringRawValueOther = "image"
+        
+        XCTAssertEqual(transformer.valueFromRawValue(stringRawValueNone), .none)
+        XCTAssertEqual(transformer.valueFromRawValue(stringRawValueBanner), .banner)
+        XCTAssertEqual(transformer.valueFromRawValue(stringRawValueVideo), .video)
+        XCTAssertEqual(transformer.valueFromRawValue(stringRawValueOther), .none)
+        
+        XCTAssertEqual(transformer.rawValueFromValue(.none), stringRawValueNone)
+        XCTAssertEqual(transformer.rawValueFromValue(.banner), stringRawValueBanner)
+        XCTAssertEqual(transformer.rawValueFromValue(.video), stringRawValueVideo)
+    }
+    
+    func testIntToEnumWithCustomInitializationTransformation() {
+        let transformer = FeatureFlagValueTransformer { (age: Int) -> AgeGroup in
+            switch age {
+            case ..<13:
+                return .under13
+            case 13...17:
+                return .between13And17
+            case 18...:
+                return .over17
+            default:
+                return .under13
+            }
+        } rawValueFromValue: { group in
+            switch group {
+            case .under13:
+                return 13
+            case .between13And17:
+                return 17
+            case .over17:
+                return 18
+            }
+        }
+        
+        let intRawValue5 = 5
+        let intRawValue15 = 15
+        let intRawValue20 = 20
+        
+        XCTAssertEqual(transformer.valueFromRawValue(intRawValue5), .under13)
+        XCTAssertEqual(transformer.valueFromRawValue(intRawValue15), .between13And17)
+        XCTAssertEqual(transformer.valueFromRawValue(intRawValue20), .over17)
+        
+        XCTAssertEqual(transformer.rawValueFromValue(.under13), 13)
+        XCTAssertEqual(transformer.rawValueFromValue(.between13And17), 17)
+        XCTAssertEqual(transformer.rawValueFromValue(.over17), 18)
+    }
+    
+}
+
+// MARK: - Support Types
+
+fileprivate enum AdType: String {
+    case none
+    case banner
+    case video
+}
+
+fileprivate enum AgeGroup {
+    case under13
+    case between13And17
+    case over17
+}
