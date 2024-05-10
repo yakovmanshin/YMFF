08bea2b14db06d26a8df9cc86788330fed3655ff
Yakov Manshin <git@yakovmanshin.com>
2024-05-10 16:15:01 +0200

    Updated `TransparentFeatureFlagStore`

--- Tests/YMFFTests/Cases/TransparentFeatureFlagStoreTests.swift
+++ Tests/YMFFTests/Cases/TransparentFeatureFlagStoreTests.swift
@@ -43,30 +43,52 @@ final class TransparentFeatureFlagStoreTests: XCTestCase {
         XCTAssertFalse(containsValue2)
     }
     
-    func test_value() async {
+    func test_value() async throws {
         store["TEST_key1"] = "TEST_value1"
         store["TEST_key2"] = "TEST_value2"
         
-        let value1: String? = await store.value(forKey: "TEST_key1")
-        let value2: Int? = await store.value(forKey: "TEST_key2")
-        let value3: Bool? = await store.value(forKey: "TEST_key3")
-        
+        let value1: String = try await store.value(forKey: "TEST_key1")
         XCTAssertEqual(value1, "TEST_value1")
-        XCTAssertNil(value2)
-        XCTAssertNil(value3)
+        
+        do {
+            let _: Int = try await store.value(forKey: "TEST_key2")
+            XCTFail("Expected an error")
+        } catch CommonFeatureFlagStoreError.typeMismatch { } catch {
+            XCTFail("Unexpected error: \(error.localizedDescription)")
+        }
+        
+        do {
+            let _: Bool = try await store.value(forKey: "TEST_key3")
+            XCTFail("Expected an error")
+        } catch CommonFeatureFlagStoreError.valueNotFound(key: let key) {
+            XCTAssertEqual(key, "TEST_key3")
+        } catch {
+            XCTFail("Unexpected error: \(error.localizedDescription)")
+        }
     }
     
-    func test_valueSync() {
+    func test_valueSync() throws {
         store["TEST_key1"] = "TEST_value1"
         store["TEST_key2"] = "TEST_value2"
         
-        let value1: String? = store.valueSync(forKey: "TEST_key1")
-        let value2: Int? = store.valueSync(forKey: "TEST_key2")
-        let value3: Bool? = store.valueSync(forKey: "TEST_key3")
-        
+        let value1: String = try store.valueSync(forKey: "TEST_key1")
         XCTAssertEqual(value1, "TEST_value1")
-        XCTAssertNil(value2)
-        XCTAssertNil(value3)
+        
+        do {
+            let _: Int = try store.valueSync(forKey: "TEST_key2")
+            XCTFail("Expected an error")
+        } catch CommonFeatureFlagStoreError.typeMismatch { } catch {
+            XCTFail("Unexpected error: \(error.localizedDescription)")
+        }
+        
+        do {
+            let _: Bool = try store.valueSync(forKey: "TEST_key3")
+            XCTFail("Expected an error")
+        } catch CommonFeatureFlagStoreError.valueNotFound(key: let key) {
+            XCTAssertEqual(key, "TEST_key3")
+        } catch {
+            XCTFail("Unexpected error: \(error.localizedDescription)")
+        }
     }
     
 }
